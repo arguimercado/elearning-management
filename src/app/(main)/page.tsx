f12bd32b6d69232a456e914ed4e681e65eb3b53f
amercado <arnold.mercado@hotmail.com>
2025-08-13 21:34:25 +0800

    Setting up shadcn

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
+      <p className="mt-4">This is a simple starter template.</p>
+    </main>
+  );
+}
