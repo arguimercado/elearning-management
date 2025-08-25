4c370ab66f22e4cfb4704f3df1e1b06a592df0bd
amercado <arnold.mercado@hotmail.com>
2025-08-25 20:01:31 +0800

    fix eslint

--- src/app/admin/courses/[id]/screen/_components/video-preview.tsx
+++ src/app/admin/courses/[id]/screen/_components/video-preview.tsx
@@ -3,7 +3,7 @@ const VideoPreview = ({ previewUrl } : {previewUrl?: string | null | undefined})
   return (
     <div>
       <iframe src={previewUrl ?? ""} title="YouTube video player" 
-        frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen className="w-full h-[500px]"></iframe>
+         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"  className="w-full h-[500px]"></iframe>
     </div>
   )
 }
