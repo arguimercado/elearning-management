b7cc588eff7d1b6d68fb8d16df1863d4a66fc8a4
amercado <arnold.mercado@hotmail.com>
2025-08-25 19:53:52 +0800

    update verify-auth

--- src/app/admin/courses/[id]/screen/_components/video-preview.tsx
+++ src/app/admin/courses/[id]/screen/_components/video-preview.tsx
@@ -1,10 +1,8 @@
-import Player from "next-video/player"
-import Video from "next-video"
 
-const VideoPreview = ({ previewUrl } : {previewUrl?: string}) => {
+const VideoPreview = ({ previewUrl } : {previewUrl?: string | null | undefined}) => {
   return (
     <div>
-      <iframe src={previewUrl} title="YouTube video player" 
+      <iframe src={previewUrl ?? ""} title="YouTube video player" 
         frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen className="w-full h-[500px]"></iframe>
     </div>
   )
